6d4b0a9418d4c530aec515804b905ce2ee84fe48
roneycm <roneychristian159@gmail.com>
2022-07-26 14:08:52 -0300

    inserting into perfil table + read

--- add.php
+++ add.php
@@ -19,17 +19,26 @@ if (!empty($dados['cad_usuario'])) {
     $add_user =  "INSERT INTO usuario (usuario, email, created) VALUES ('$user', '$email', NOW())";
     // trying to insert in multiple tables with mysqli but can't insert into perfil table yet
     if (mysqli_query($conn, $add_user) === TRUE) {
+        // get last inserted id
         $cad_id = mysqli_insert_id($conn);
-        $id_cad = $cad_id;
+        // insert into endereco table
         $add_info = "INSERT INTO endereco (rua,numero,bairro,usuario_id) VALUES ('$rua', '$numero', '$bairro', '$cad_id')";
-        $add_perfil .= "INSERT INTO perfil (bio, idade, profissao, pusuario_id) VALUES ('$bio', '$idade', '$prof', '$id_cad')";
-        
-        if (mysqli_query($conn, $add_info) === TRUE) {
-            mysqli_query($conn, $add_perfil);
+        $query_info = $conn->prepare($add_info);
+        $query_info->execute();
+
+        // insert into perfil table
+        $add_perfil = "INSERT INTO perfil (bio, idade, profissao, pusuario_id) VALUES ('$bio', '$idade', '$prof', '$cad_id')";
+        $query_perfil = $conn->prepare($add_perfil);
+
+        // show succes message if success
+        if ($query_perfil->execute()) {
             $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
             header('Location: index.php');
             }
         }
+    
+    $query_info->close();
+    $query_perfil->close();
     
     if (!mysqli_commit($conn)) {
         $_SESSION['msg'] = "<p style='color:red;'>Não foi possível cadastrar!</p>";
--- consult.php
+++ consult.php
@@ -3,11 +3,11 @@ include_once('connection.php');
 require_once 'fpdf184/fpdf.php';
 
 
-$get_user = "SELECT * FROM usuario as u LEFT JOIN endereco as en ON en.usuario_id=u.id";
+$get_user = "SELECT * FROM usuario as u LEFT JOIN endereco as en ON en.usuario_id=u.id LEFT JOIN perfil as p ON u.id = p.pusuario_id";
 $result_user = mysqli_query($conn, $get_user);
 
 $pdf = new FPDF();
-$pdf->AddPage();
+$pdf->AddPage('H');
 
 $pdf->SetFont('Arial', 'B', 18);
 $pdf->Cell(190, 10, utf8_decode("Usuários cadastrados"), 0, 0, 'C');
@@ -19,6 +19,9 @@ $pdf->Cell(60, 7, utf8_decode("Email"), 1, 0, 'C');
 $pdf->Cell(40, 7, utf8_decode("Rua"), 1, 0, 'C');
 $pdf->Cell(20, 7, utf8_decode("Nº"), 1, 0, 'C');
 $pdf->Cell(30, 7, utf8_decode("Bairro"), 1, 0, 'C');
+$pdf->Cell(40, 7, utf8_decode("Bio"), 1, 0, 'C');
+$pdf->Cell(20, 7, utf8_decode("Idade"), 1, 0, 'C');
+$pdf->Cell(40, 7, utf8_decode("Profissao"), 1, 0, 'C');
 $pdf->Ln();
 
 while ($user_get = $result_user->fetch_assoc()) {
@@ -28,6 +31,9 @@ while ($user_get = $result_user->fetch_assoc()) {
     $pdf->Cell(40, 7, utf8_decode($user_get['rua']), 1, 0, 'C');
     $pdf->Cell(20, 7, utf8_decode($user_get['numero']), 1, 0, 'C');
     $pdf->Cell(30, 7, utf8_decode($user_get['bairro']), 1, 0, 'C');
+    $pdf->Cell(40, 7, utf8_decode($user_get['bio']), 1, 0, 'C');
+    $pdf->Cell(20, 7, utf8_decode($user_get['idade']), 1, 0, 'C');
+    $pdf->Cell(40, 7, utf8_decode($user_get['profissao']), 1, 0, 'C');
     $pdf->Ln();
 }
 
